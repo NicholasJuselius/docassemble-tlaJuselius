1f4f743de8c4af01bf2a3e9258c5bc8b35cd4048
Nicholas Juselius <71146004+NicholasJuselius@users.noreply.github.com>
2020-10-09 01:01:42 -0400

    Assign 4

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.tlaJuselius',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='Assignment 3',
+      long_description='Assignment 4',
       long_description_content_type='text/markdown',
       author='Nicholas Juselius',
       author_email='juselius@ualberta.ca',
